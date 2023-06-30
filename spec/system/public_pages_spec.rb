1cab7276b952ac96cfe0734e415a6c42e6865aa5
Cezar Halmagean <cezar@mixandgo.com>
2023-06-30 10:24:45 +0300

    Changed the test driver to Selenium Chrome

--- spec/system/public_pages_spec.rb
+++ spec/system/public_pages_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "PublicPages", type: :system do
+RSpec.describe "Public pages", type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by(:selenium, using: :chrome, screen_size: [1400, 1400])
   end
 
   pending "add some scenarios (or delete) #{__FILE__}"
